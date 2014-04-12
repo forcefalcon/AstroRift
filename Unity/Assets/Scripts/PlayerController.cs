dfc29cf817ea1f1f1e79c5c1696a6bb3e8341580
Pat Lalonde <plalonde@gmail.com>
2014-04-12 15:05:04 -0400

    Hooking up PlayerController with Oculus camera.

---
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	/*
+	 * Rotation
+	 */	
+	public float RotationSensitivityX = 15F;
+	public float RotationSensitivityY = 15F;
+	public float RotationSensitivityZ = 15F;
+	
+	/*
+	 * Movement
+	 */
+	public float MovementSensitivityX = 1F;
+	public float MovementSensitivityY = 1F;
+	public float MovementSensitivityZ = 1F;
+	
+	void Update()
+	{
+		UpdateRotation();
+		UpdateMovement();
+	}
+	
+	void UpdateRotation()
+	{
+		Quaternion rotX = Quaternion.AngleAxis(Input.GetAxis("Right X") * RotationSensitivityX * Time.deltaTime, Vector3.up);
+		Quaternion rotY = Quaternion.AngleAxis(Input.GetAxis("Right Y") * RotationSensitivityY * Time.deltaTime, Vector3.left);
+		
+		float angZ = Input.GetButton("Left Bumper") ? RotationSensitivityZ : 0.0f;
+		angZ -= Input.GetButton("Right Bumper") ? RotationSensitivityZ : 0.0f;
+		angZ *= Time.deltaTime;
+		Quaternion rotZ = Quaternion.AngleAxis(angZ, Vector3.forward);
+		
+		transform.localRotation = transform.localRotation * rotX * rotY * rotZ;
+	}
+	
+	void UpdateMovement()
+	{
+		transform.Translate(new Vector3(Input.GetAxis("Left X") * MovementSensitivityX * Time.deltaTime, -1.0f * Input.GetAxis("Triggers") * MovementSensitivityZ * Time.deltaTime, -1.0f * Input.GetAxis("Left Y") * MovementSensitivityY * Time.deltaTime), Space.Self);
+	}
+}
