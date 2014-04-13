9c79924ae6935261af3de6731195700ac70b1be3
Pat Lalonde <plalonde@gmail.com>
2014-04-12 21:11:05 -0400

    - Removing Sun prefab. - Adding Planets prefab. - Rotating Planets.

---
+++ Unity/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour
+{
+	public Vector3 Axis = Vector3.up;
+	public float AngularSpeed = 60.0f;
+	
+	void Awake()
+	{
+		Axis.Normalize();
+	
+		transform.localRotation = Quaternion.FromToRotation(Vector3.up, Axis);
+	}
+	
+	void Update()
+	{
+		transform.localRotation *= Quaternion.AngleAxis(AngularSpeed * Time.deltaTime, Axis);
+	}
+}
